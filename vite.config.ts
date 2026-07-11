8351729f745bcafb60c43895f0ea848bdfd71b4d
Halfmiann <1694371125@qq.com>
2026-07-12 00:32:37 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from '@lark-apaas/coding-preset-vite-react'
 
 export default defineConfig({
+  // 强制替换模板变量，线上不会再出现{{appName}}
+  define: {
+    __APP_NAME: JSON.stringify("Terminal BETA6"),
+    'import.meta.env.appName': JSON.stringify("Terminal BETA6")
+  }
 })

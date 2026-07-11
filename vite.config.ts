37db69405cd7f03a19678629b5c868bf232795b4
Halfmiann <1694371125@qq.com>
2026-07-12 00:00:43 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
-import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from '@lark-apaas/coding-preset-vite-react'
+
 export default defineConfig({
   base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@shared': path.resolve(__dirname, 'shared'),
-    },
-  },
 })
